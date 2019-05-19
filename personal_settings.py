92a38d3cc019e795eac67b8163d400e2cdd3bb0b
Lang Shao <lang@pmo.ac.cn>
2019-05-19 12:27:00 +0800

    add personal_settings.py

---
+++ personal_settings.py
@@ -0,0 +1,11 @@
+# modify this file for personal settings 
+# the changes of which should be ignored during each commit
+
+#databasedir='/home/lang/work/GBM/burstdownload/data/'
+get_databasedir():
+	databasedir='/diskb/Database/Fermi/gbm_burst/data/'
+	return databasedir
+
+get_ncore():
+	ncore=80
+	return ncore
