02b73506ab0214719b3197135e4a100c1c5da4ca
Lang-Shao <lang@pmo.ac.cn>
2019-05-21 10:36:36 +0800

    include personal_setting.py

---
+++ personal_settings.py
@@ -0,0 +1,12 @@
+# modify this file for personal settings 
+# the changes of which should be ignored during each commit
+
+
+def get_databasedir():
+	databasedir='/home/lang/work/GBM/burstdownload/data/'
+	#databasedir='/diskb/Database/Fermi/gbm_burst/data/'
+	return databasedir
+
+def get_ncore():
+	ncore=3
+	return ncore
