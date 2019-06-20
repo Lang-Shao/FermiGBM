8d1363bebb073e9da9c17bd28a502d1adca1b4b2
Lang-Shao <lang@pmo.ac.cn>
2019-06-21 06:54:24 +0800

    fix issue

--- GBM_catalog_spectralanalysis_lib.py
+++ GBM_catalog_spectralanalysis_lib.py
@@ -921,7 +921,7 @@ class GRB:
 					break
 				filelink = filelist[0]
 				qsj, pos = find_right_list(filelink, t_met)
-				if qsj == None:
+				if type(qsj) == type(None):
 					break
 				myGBM = GBM(qsj,pos*u.m,timestrisot)		
 				fig = plt.figure(figsize=(20,10))
